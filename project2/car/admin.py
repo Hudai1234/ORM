a76f52403b216f650c68ac5d43bc506f64582073
Hudai1234 <hudaifatajudeen@gmail.com>
2025-09-17 13:40:44 +0530

    Success

---
+++ project2/car/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from . models import car
+
+# Register your models here.
+admin.site.register(car)
+class carAdmin(admin.ModelAdmin):
+    list_display = ('id','brand','model','year','price')
